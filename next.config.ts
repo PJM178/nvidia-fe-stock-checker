88ff0c52444f44a0ad985215c23f33699fa777e7
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-02-25 21:53:38 +0200

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "nvidia-fe-stock-checker",
+  basePath: "/nvidia-fe-stock-checker",
   images: {
     unoptimized: true,
   },
