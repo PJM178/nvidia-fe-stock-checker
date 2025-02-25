2c5c8d1f13554a0c96684d2b5bf055157ca1aa48
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-02-25 21:33:26 +0200

    test github pages deploying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: "export",
+  basePath: "nvidia-fe-stock-checker",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
